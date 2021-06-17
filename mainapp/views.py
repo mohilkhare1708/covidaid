78f3e68abca5fb11f14613a7449116cbdf3b5345
mohilkhare1708 <mohilkhare17@gmail.com>
2021-06-17 23:56:27 +0530

    cleanup

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,31 +13,6 @@ access_token_secret = os.getenv('access_token_secret')
 consumer_key = os.getenv('consumer_key')
 consumer_secret = os.getenv('consumer_secret')
 
-# class TwitterStreamer():
-#     def __init__(self):
-#         pass
-
-#     def stream_tweets(self, hash_tag_list):
-#         listener = StdOutListener()
-#         auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
-#         auth.set_access_token(access_token,access_token_secret)
-#         stream = tweepy.Stream(auth, listener)
-#         stream.filter(track=['oxygen', 'remdesivir'])#hash_tag_list)
-
-# class StdOutListener(StreamListener):
-#     def on_data(self, data):
-#         try:
-#             data = json.loads(data)
-#             print(data['text'])
-#             return True
-#         except BaseException as e:
-#             print("Error on_data %s" % str(e))
-#         return True
-          
-
-#     def on_error(self, status):
-#         print(status)
-
 def home_page(request):
     return render(request, 'mainapp/home.html', {'title': 'CovidAid - A helping hand to the help needers and providers'})
 
